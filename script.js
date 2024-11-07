1d817fa5abc32932e3a834fc1ac69f34a07433f5
Surya Annadurai <suryaannadurai1041999@gmail.com>
2024-11-07 23:40:33 +0530

    stopwatch created

---
+++ script.js
@@ -0,0 +1,12 @@
+let min = document.querySelector(".box1")
+let sec = document.querySelector(".box2")
+let milli = document.querySelector(".box3")
+
+
+let start = document.querySelector(".start");
+// console.log(start)
+document.addEventListener("click" , (event) =>{
+    if (event.target.classList.contains("start")){
+        
+    }
+})
